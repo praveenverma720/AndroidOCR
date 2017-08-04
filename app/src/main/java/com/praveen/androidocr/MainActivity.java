b5dd1f5977dfe5a5fb9db7800604b86090f6158a
Praveen Kumar <praveenverma720@gmail.com>
2017-08-04 19:07:02 +0530

    First Commit

---
+++ app/src/main/java/com/praveen/androidocr/MainActivity.java
@@ -0,0 +1,158 @@
+package com.praveen.androidocr;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String log = "app error found";
+    private static final int photo_request = 20;
+    private TextView scanResults;
+    private Uri imageUri;
+    private TextRecognizer detector;
+    private static final int write_premission = 10;
+    private static final String uri = "uri";
+    private static final String saved_result = "result";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        scanResults = (TextView) findViewById(R.id.results);
+        if (savedInstanceState != null) {
+            imageUri = Uri.parse(savedInstanceState.getString(uri));
+            scanResults.setText(savedInstanceState.getString(saved_result));
+        }
+
+        detector = new TextRecognizer.Builder(getApplicationContext()).build();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(MainActivity.this, new
+                        String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, write_premission);
+            }
+        });
+    }    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case write_premission:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                } else {
+                    Toast.makeText(MainActivity.this, "permission failed", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == photo_request && resultCode == RESULT_OK) {
+            launchMediaScanIntent();
+            try {
+                Bitmap bitmap = decodeBitmapUri(this, imageUri);
+                if (detector.isOperational() && bitmap != null) {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> textBlocks = detector.detect(frame);
+                       String blocks = "";
+                    String lines = "";
+                    String words = "";
+                    for (int index = 0; index < textBlocks.size(); index++) {
+                        TextBlock tBlock = textBlocks.valueAt(index);
+                        blocks = blocks + tBlock.getValue() + "\n" + "\n";
+                        for (Text line : tBlock.getComponents()) {
+                            lines = lines + line.getValue() + "\n";
+                            for (Text element : line.getComponents()) {
+                                words = words + element.getValue() + ", ";
+                            }
+                        }
+                    }
+                    if (textBlocks.size() == 0) {
+                           scanResults.setText("scan failed: not found any image");
+                    } else {
+                         scanResults.setText(scanResults.getText() + "Blocks: " + "\n");
+                        scanResults.setText(scanResults.getText() + blocks + "\n");
+                        scanResults.setText(scanResults.getText() + "---------" + "\n");
+                        scanResults.setText(scanResults.getText() + "Lines: " + "\n");
+                        scanResults.setText(scanResults.getText() + lines + "\n");
+                        scanResults.setText(scanResults.getText() + "---------" + "\n");
+                        scanResults.setText(scanResults.getText() + "Words: " + "\n");
+                        scanResults.setText(scanResults.getText() + words + "\n");
+                        scanResults.setText(scanResults.getText() + "---------" + "\n");
+                    }
+                } else {
+                    scanResults.setText("detector not working");
+                }
+            } catch (Exception e) {
+                Toast.makeText(this, "faild to load image", Toast.LENGTH_SHORT)
+                        .show();
+                Log.e(log, e.toString());
+            }
+        }
+    }
+
+    private void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "image.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, photo_request);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (imageUri != null) {
+            outState.putString(uri, imageUri.toString());
+            outState.putString(saved_result, scanResults.getText().toString());
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void launchMediaScanIntent() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException {
+        int targetW = 600;
+        int targetH = 600;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(ctx.getContentResolver().openInputStream(uri), null, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeStream(ctx.getContentResolver()
+                .openInputStream(uri), null, bmOptions);
+    }
+}
